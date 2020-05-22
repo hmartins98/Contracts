a7df65cd5471503f49317688f5b6009a1939e4e6
hmartins98 <hugo.alex.val.martins@hotmail.com>
2020-05-22 14:37:23 +0100

    first commit

--- generate.sh
+++
@@ -1,2 +0,0 @@
-cd ~\go\src
-protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/Auth/Auth.proto github.com/hmartins98/Contracts/Customer/Customer.proto github.com/hmartins98/Contracts/CustomTypes/CustomTypes.proto
