424c3fff7c83f5382ec92fdd229b1401bf4696b3
hmartins98 <hugo.alex.val.martins@hotmail.com>
2020-06-20 14:48:42 +0100

    CMS first commit

--- generate.sh
+++ generate.sh
@@ -1,2 +1,4 @@
 cd ~\go\src
-protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/Auth/Auth.proto github.com/hmartins98/Contracts/CustomTypes/CustomTypes.proto
+protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/Auth/Auth.proto
+protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/CustomTypes/CustomTypes.proto
+protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/CMS/CMS.proto
