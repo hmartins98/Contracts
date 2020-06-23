158e50fac789a1a8a36f79d7a51115be03cc0e2e
hmartins98 <hugo.alex.val.martins@hotmail.com>
2020-06-23 13:56:53 +0100

    update button to invalidate cache

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
+# Go
 cd ~\go\src
 protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/Auth/Auth.proto
 protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/CustomTypes/CustomTypes.proto
