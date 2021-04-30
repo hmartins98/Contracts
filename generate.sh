4136adb8d87f4e479b7941e8be41545050ec86fb
hmartins98 <hugo.alex.val.martins@hotmail.com>
2021-04-30 22:36:36 +0100

    Created API Contract

--- generate.sh
+++ generate.sh
@@ -3,3 +3,4 @@ cd ~\go\src
 protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/Auth/Auth.proto
 protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/CustomTypes/CustomTypes.proto
 protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/CMS/CMS.proto
+protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/API/API.proto
