c686d32a174139694bbe8ef22dc72b0cb4613f66
hmartins98 <hugo.alex.val.martins@gmail.com>
2022-04-20 11:41:17 +0100

    update to bash

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 # Go
-cd ~\go\src
-protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/Auth/Auth.proto
-protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/CustomTypes/CustomTypes.proto
-protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/CMS/CMS.proto
-protoc --go_out=plugins=grpc:. github.com/hmartins98/Contracts/API/API.proto
+cd ~
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative github.com/hmartins98/Contracts/CustomTypes/CustomTypes.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative github.com/hmartins98/Contracts/Auth/Auth.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative github.com/hmartins98/Contracts/CMS/CMS.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative github.com/hmartins98/Contracts/API/API.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative github.com/hmartins98/Contracts/Items/Items.proto
